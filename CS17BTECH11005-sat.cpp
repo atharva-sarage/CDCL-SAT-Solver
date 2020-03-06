8636430a4a86714f5dd8b8f4ddd790cfc354506c
atharva-sarage <atharva.sarage@gmail.com>
2020-03-06 14:27:40 +0530

    final submission

--- CS17BTECH11005-sat.cpp
+++ CS17BTECH11005-sat.cpp
@@ -4,8 +4,7 @@
 ******************************************/
 #include<bits/stdc++.h>
 using namespace std;
-//#define OUTPUT
-// Global Variables for storing finalAssignment and totalNumberofClauses and Variables
+// Global Variables
 struct literalState{
     int assigned;  // value assigned to literal
     int level;     // the level of search tree at which this literal was assigned
@@ -23,6 +22,7 @@ int threshold=100; // hyperparameter for restart after threshold many conflicts
  */
 // 
 set<pair<int,int>>variableActivity; 
+int satisfiedVariables=0; // counter for total number of satisfied variables
 /**
  * level0Variables
  * all the literals which are true at level 0.
@@ -32,7 +32,13 @@ set<pair<int,int>>variableActivity;
  */
 vector<int>level0Literals;
 vector<bool>finalAssignment;// stores the finalAssignment of the literals
-
+/**
+ * unitLiterals store the unitliteral which will be resolved during unitpropogation.
+ * It is populated before the call to unitPropogation where it is used.Before returing
+ * from unitPropogation it clears this vector.
+ * If it is at level 0 then it is equal to level0Variables
+ * If it is at any other level then it is equal decision taken at that level
+ */
 vector<int>unitLiterals;// stores the unitLiterals
 
 vector <literalState> state;     // to store state of all literals
@@ -86,7 +92,7 @@ class clause{
                 tautology|=(find(literals.begin(),literals.end(),-2*x-1)!=literals.end());
             }
         }
-        // Returns total Literals
+        // Returns total Literals in a clause
         int getCountLiteralsInClause(){
             return totalLiterals;
         }
@@ -98,6 +104,7 @@ class clause{
         // where one clause contains c and other contains negation c
         static clause resolution(clause a,clause b,int c){
             //visited- to keep track of which literals are added to resoluted clause
+            // initiall set all to false
             vector<bool> visited(2*totalVariables+5,false);
             clause resolutedClause; // 
             for(auto lit:a.literals){
@@ -131,13 +138,20 @@ class clause{
 class clauseSet{
     public:
         vector <clause> clauses; // vector of clause
-        // for each literal we keep a pointer to a list(unordered set) 
-        // which stores which clauses is this literal watching.        
+        /**
+         * Watched Literals Data Structures
+         * literalClauseMap and watchedLit are the data structures used to implement 
+         * 2 watched literals.
+         * literalClauseMap:-
+         * for each literal we keep a pointer to a list(container is unordered set) 
+         * which stores, which clauses is this literal watching.
+         * watchedLit:-
+         * for each clause we keep 2 unassigned literals.
+         * watchedLit[1] gives a pair, the 2 watched literals for clause 1
+         */
         vector<unordered_set<int>*> literalClauseMap;  
-
-        // for each clause we keep 2 unassigned literals.
-        // watchedLit[1] gives a pair, the 2 watched literals for cluase 1        
         vector<pair<int,int>>watchedLit;    
+
         // state stores the initial state of all the clauses
         clauseSet(){                 
             // clauses are also 1 indexed put a dummy clause at 0 index
@@ -155,6 +169,7 @@ class clauseSet{
         void addClause(clause cs){
             /**
              * Update the activity of variables
+             * Initial activity is the occurance of that variable
              */
             for(auto lit:cs.literals){
                 if(state[lit].assigned==true)
@@ -188,7 +203,7 @@ class clauseSet{
                     /**
                      * We detect a unitClause if under current assignment we are
                      * able to find only 1 watched literal. If this happens then we 
-                     * add the other watched literal to unitLiterals as it must be set to true.
+                     * add this literal to unitLiterals as it must be set to true.
                      */
                     int flag=0,idx1,idx2=-1,lit1,lit2=-1,idx3,lit3;
                     for(int i=0;i<cs.literals.size();i++){
@@ -236,13 +251,7 @@ class clauseSet{
             return false;
         }
 };
-/**
- * SATsolver Class
- * Stores a pointer to clauseset object to access literalMap and all the clauses  
- */
-int iter=0;
 vector<clause>temporaryBuffer; // to store newly learned clause
-int satisfiedVariables=0; // counter for total number of satisfied variables
 /**
  * unSetInformation function unsets all literals provided as argument.
  * assigned to false,depth to 0,antclause to 0
@@ -384,8 +393,7 @@ pair<bool,int>unitPropogation(vector<int>&unsetLiterals,int level,clauseSet* cla
         vector <int> eraseList;
         if(clauseset->literalClauseMap[compUnitLiteral]->size()>0){  
             // iterate over all the clauses containing complement of that literal                       
-            for(auto clauseNum:*(clauseset->literalClauseMap[compUnitLiteral])){
-                iter++;
+            for(auto clauseNum:*(clauseset->literalClauseMap[compUnitLiteral])){                
                 // if the clause is satisfied then move to next clause
                 if(clauseset->isSatisfied(clauseNum))continue;
                 // idx1 and idx2 are index corresponding to the watched literals in current clause
@@ -405,7 +413,7 @@ pair<bool,int>unitPropogation(vector<int>&unsetLiterals,int level,clauseSet* cla
                         if(state[clauseset->clauses[clauseNum].literals[i]].assigned==false && 
                             state[complement(clauseset->clauses[clauseNum].literals[i])].assigned==false)
                             {
-                                // make updates
+                                // make updates to watched Literal data structures
                                 clauseset->watchedLit[clauseNum].first=i;
                                 clauseset->literalClauseMap[clauseset->clauses[clauseNum].literals[i]]->insert(clauseNum);
                                 // now this clauseNum must be erased from list of of clauses watched by compliteral
@@ -456,6 +464,10 @@ pair<bool,int>unitPropogation(vector<int>&unsetLiterals,int level,clauseSet* cla
         return {true,-1};
     return {true,0}; // no conflict in unitPropogation go to deduce stage
 }
+/**
+ * SATsolver Class
+ * Stores a pointer to clauseset object to access literalMap and all the clauses  
+ */
 class SATsolver{
     private:        
         clauseSet* clauseset; // a pointer to clauseset object
@@ -465,14 +477,15 @@ class SATsolver{
          * unitLiterals stores all the literals which are true at this level.
          * If it is level 0- then it is same as level0Variable(the varibles which are true at level 0)         
          * Else - it contains decion literal which was taken at this level.
+         * 
          * Step1: unitLiterals vector has the all the unitLiterals which are true at level(argument of cdcl function).         * 
-         * Step2: do unitPropogation at this level if it return true then we are cool no conflict occured
+         * Step2: Do unitPropogation at this level if it return true then we are cool no conflict occured
          *         If it returned false then we cdcl returns false and backtracks to level set by unitpropogation
          * Step3: We pick a new variable that is not assigned till now and call cdcl(level+1)
          *        and call cdcl(level+1)
          *        now if it returns true we are done formula is SAT
-         *        If it returns false and orderes to jump at lower level we unsetLiterals all the information that
-         *        was taken at this level and backtrack         
+         *        If it returns false and orderes to jump at lower level we unset all the information that
+         *        was taken at this level and backtrack to appropriate level       
          *        It it tells us to stay at same level. Add new learned clause.
          *        Do unitpropogation with the same decision literal which was taken at this level to STEP 2         
          *
@@ -526,9 +539,14 @@ class SATsolver{
                 // unitPropogation will handle it.       
                 unitLiterals.insert(unitLiterals.begin(),bestLiteral);
                 /**
-                 * store the literal which was taken at this level but it is set in level+1
-                 * we are level d then we choose lit call dpll for d+1 and set the literal 
-                 * at level d+1.
+                 * Imagine we are at middle point of the edge that connects level d 
+                 * to level d+1. At this point we decide what should be the next decision 
+                 * variable. So this new variable we pick is decision variable for level d+1.
+                 * NOTE: decision for level d has already been taken and we need to take
+                 * decision for d+1.
+                 * unitLiterals will hold this decision variable.
+                 * Which is true(since it is a unitLiteral and inference will be done
+                 * while unitPropogation function in cdcl function )
                  */
                 decisionLiteral[level+1]=bestLiteral;
                 // make a call to cdcl 
@@ -540,13 +558,15 @@ class SATsolver{
                 // discard current decision variable taken at this level(best literal)
                 decisionLiteral[level+1]=0; 
                 /**
-                 * discard unitpropogation information which is stored in either
+                 * Discard unitpropogation information:
+                 * It is stored in either
                  * unsetLiterals(first unitPropogation at this level)
                  * unsetLiterals2(any other unitPropogation at this level)
                  * it may happen that we learn clause at keep comming back to same 
                  * level. When this happens for the first time unsetLiterals has the
-                 * information any subsequent unitpropogatino information is 
-                 * present in unsetLiterals2.          * 
+                 * information any subsequent unitpropogation information is 
+                 * present in unsetLiterals2.          *
+                 * So we need to clear this information so we call unsetInformation 
                  */
                 unSetInformation(unsetLiterals); 
                 unSetInformation(unsetLiterals2); 
@@ -605,6 +625,7 @@ int main(){
         cin>>ch;
     }
     cin>>str>>totalVariables>>totalClauses;
+    // resize vectors to save time
     state.resize(2*totalVariables+5);
     depth.resize(totalVariables+5);
     decisionLiteral.resize(totalVariables+5);
@@ -639,15 +660,15 @@ int main(){
                 cout<<getvariable(i)<<" ";     
         }
         cout<<"0\n";   
-        for(auto k:clauses.clauses){
-        bool set=false;
-        for(auto lit: k.literals){
-            if(lit==0)
-                {set=true;break;}                         
-            set|=state[lit].assigned;
-        }
-        assert(set==true);
-    }   
+        // for(auto k:clauses.clauses){
+        // bool set=false;
+        // for(auto lit: k.literals){
+        //     if(lit==0)
+        //         {set=true;break;}                         
+        //     set|=state[lit].assigned;
+        // }
+        // assert(set==true);
+        //}   
     }
     return 0; 
 }
